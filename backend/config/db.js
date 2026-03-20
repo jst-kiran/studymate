ef9d2443615182e3cf9f2647df7c9e656de5a303
Kiran Suthar <kiransuthar7874@gmail.com>
2026-03-20 19:02:05 +0530

    fix railway env variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 const mysql2 = require('mysql2');
 
 const pool = mysql2.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '1234',
-  database: process.env.DB_NAME || 'studymate',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool.promise();
